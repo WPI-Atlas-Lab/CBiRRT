5641f55910a7980854787e9cb0e68f8630f485a1
Xianchao <xianchao@xSystem.(none)>
2014-09-26 11:41:03 -0400

    modified atlas_reaching

--- test/atlas_reaching.py
+++ test/atlas_reaching.py
@@ -87,58 +87,58 @@ if __name__ == "__main__":
     robot.SetActiveDOFs(activejoint)
     robot.SetActiveDOFValues([0.0, -1.3, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0])
 
-#     ################ IK solver #######################
-#     # #define targets for hand 
-#     # handtrans = mat([0.82, 0.21, 1.23373]).T
-#     # handrot = mat(rodrigues([pi/2, 0, pi/2]))
-#     # Tik = MakeTransform(handrot,handtrans);
-#     # print Tik
-#     # startik = probs_cbirrt.SendCommand('DoGeneralIK exec nummanips 0 maniptm 1 %s'%SerializeTransform(Tik))
-#     # print robot.GetActiveDOFValues()
-#     startik = "-2.56229520e-01 -9.15233016e-01 1.66518712e+00 1.59568906e+00 9.34448361e-01 6.38563752e-01 0.0 0.0 0.0"
+    ################ IK solver #######################
+    # #define targets for hand 
+    # handtrans = mat([0.82, 0.21, 1.23373]).T
+    # handrot = mat(rodrigues([pi/2, 0, pi/2]))
+    # Tik = MakeTransform(handrot,handtrans);
+    # print Tik
+    # startik = probs_cbirrt.SendCommand('DoGeneralIK exec nummanips 0 maniptm 1 %s'%SerializeTransform(Tik))
+    # print robot.GetActiveDOFValues()
+    startik = "-2.56229520e-01 -9.15233016e-01 1.66518712e+00 1.59568906e+00 9.34448361e-01 6.38563752e-01 0.0 0.0 0.0"
 
-#     #plan a reaching motion to grab the box
-#     robot.SetActiveDOFValues([0.0, -1.3, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0])
-#     probs_cbirrt.SendCommand('RunCBiRRT jointgoals %d %s'%(len(str2num(startik)),Serialize1DMatrix(mat(str2num(startik)))))
+    #plan a reaching motion to grab the box
+    robot.SetActiveDOFValues([0.0, -1.3, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0])
+    probs_cbirrt.SendCommand('RunCBiRRT jointgoals %d %s'%(len(str2num(startik)),Serialize1DMatrix(mat(str2num(startik)))))
    
-#     #execute the planned trajectory 
-# #    orEnv.LockPhysics(False)
-#     probs_cbirrt.SendCommand('traj cmovetraj.txt')
-#     robot.WaitForController(0)
-# #    orEnv.LockPhysics(True)
+    #execute the planned trajectory 
+#    orEnv.LockPhysics(False)
+    probs_cbirrt.SendCommand('traj cmovetraj.txt')
+    robot.WaitForController(0)
+#    orEnv.LockPhysics(True)
     
 
-    #let's define two TSR chains for this task, they differ only in the rotation of the hand
+    # #let's define two TSR chains for this task, they differ only in the rotation of the hand
 
-    #first TSR chain
+    # #first TSR chain
 
-    #place the first TSR's reference frame at the object's frame relative to world frame
-    T0_w = MakeTransform(mat(eye(3)),mat([0.85, 0.2, 1.25]).T)
+    # #place the first TSR's reference frame at the object's frame relative to world frame
+    # T0_w = MakeTransform(mat(eye(3)),mat([0.85, 0.2, 1.25]).T)
 
-    #get the TSR's offset frame in w coordinates
-    Tw_e1 = MakeTransform(rodrigues([pi/2, 0, 0]),mat([0.4, 0.0, 0.1]).T)
+    # #get the TSR's offset frame in w coordinates
+    # Tw_e1 = MakeTransform(rodrigues([pi/2, 0, 0]),mat([0.4, 0.0, 0.1]).T)
 
-    #define bounds to only allow rotation of the hand about z axis and a small deviation in translation along the z axis
-    Bw = mat([0, 0,   0, 0,   -0.02, 0.02,   0, 0,   0, 0,   -pi, pi])
+    # #define bounds to only allow rotation of the hand about z axis and a small deviation in translation along the z axis
+    # Bw = mat([0, 0,   0, 0,   -0.02, 0.02,   0, 0,   0, 0,   -pi, pi])
 
-    TSRstring1 = SerializeTSR(0,'NULL',T0_w,Tw_e1,Bw)
-    TSRChainString1 = SerializeTSRChain(0,1,0,1,TSRstring1,'NULL',[])
+    # TSRstring1 = SerializeTSR(0,'NULL',T0_w,Tw_e1,Bw)
+    # TSRChainString1 = SerializeTSRChain(0,1,0,1,TSRstring1,'NULL',[])
 
 
-    #now define the second TSR chain
-    #it is the same as the first TSR Chain except Tw_e is different (the hand is rotated by 180 degrees about its z axis)
-    Tw_e2 = MakeTransform(rodrigues([0, pi, 0])*rodrigues([pi/2, 0, 0]),mat([0.4, 0.0, 0.1]).T)
-    TSRstring2 = SerializeTSR(0,'NULL',T0_w,Tw_e2,Bw)
-    TSRChainString2 = SerializeTSRChain(0,1,0,1,TSRstring2,'NULL',[])
+    # #now define the second TSR chain
+    # #it is the same as the first TSR Chain except Tw_e is different (the hand is rotated by 180 degrees about its z axis)
+    # Tw_e2 = MakeTransform(rodrigues([0, pi, 0])*rodrigues([pi/2, 0, 0]),mat([0.4, 0.0, 0.1]).T)
+    # TSRstring2 = SerializeTSR(0,'NULL',T0_w,Tw_e2,Bw)
+    # TSRChainString2 = SerializeTSRChain(0,1,0,1,TSRstring2,'NULL',[])
 
-    #call the cbirrt planner, it will generate a file with the trajectory called 'cmovetraj.txt'
-    resp = probs_cbirrt.SendCommand('RunCBiRRT psample 0.25 %s %s'%(TSRChainString1,TSRChainString2))
-    probs_cbirrt.SendCommand('traj cmovetraj1.txt')
+    # #call the cbirrt planner, it will generate a file with the trajectory called 'cmovetraj.txt'
+    # resp = probs_cbirrt.SendCommand('RunCBiRRT psample 0.25 %s %s'%(TSRChainString1,TSRChainString2))
+    # probs_cbirrt.SendCommand('traj cmovetraj1.txt')
 
-    #    traj=RaveCreateTrajectory(orEnv,'BarrettWAM')
-    #    traj.Read('cmovetraj.txt',robot)
-    #    robot.GetController().SetPath(traj)
-    robot.WaitForController(0)
+    # #    traj=RaveCreateTrajectory(orEnv,'BarrettWAM')
+    # #    traj.Read('cmovetraj.txt',robot)
+    # #    robot.GetController().SetPath(traj)
+    # robot.WaitForController(0)
 
 
     print "Press return to exit."
